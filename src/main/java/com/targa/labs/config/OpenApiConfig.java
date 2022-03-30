4bbc3fb17fd35708c7f249d1402f2cf5f177267e
fayca121 <bz.faycal@gmail.com>
2022-03-30 20:55:25 +0200

    add auth to openapi

--- src/main/java/com/targa/labs/config/OpenApiConfig.java
+++ src/main/java/com/targa/labs/config/OpenApiConfig.java
@@ -1,2 +1,32 @@
-package com.targa.labs.config;public class OpenApiConfig {
+package com.targa.labs.config;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+
+import javax.ws.rs.core.Application;
+
+@SecurityScheme(
+        securitySchemeName = "jwt",
+        description = "JWT authentication with bearer token",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "Bearer [token]")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "QuarkuShop API",
+                description = "Sample application for the book 'Playing with Java Microservices with Quarkus and Kubernetes'",
+                contact = @Contact(name = "Bououza Faycal",
+                        email = "bz.faycal@gmail.com",
+                        url = "https://blog.nebrass.fr"),
+                version = "1.0.0-SNAPSHOT"
+        ),
+        security = @SecurityRequirement(name = "JWT")
+)
+public class OpenApiConfig extends Application {
 }
