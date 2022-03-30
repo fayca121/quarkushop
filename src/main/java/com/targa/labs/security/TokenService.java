2c9993081f7409a7f35ceb9a22f9c7fb1156db4d
fayca121 <bz.faycal@gmail.com>
2022-03-30 20:54:46 +0200

    add user resource to manager jwt

--- src/main/java/com/targa/labs/security/TokenService.java
+++ src/main/java/com/targa/labs/security/TokenService.java
@@ -1,2 +1,60 @@
-package com.targa.labs.security;public class TokenService {
+package com.targa.labs.security;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.security.UnauthorizedException;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@RequestScoped
+public class TokenService {
+    @ConfigProperty(name="mp.jwt.verify.issuer",defaultValue = "undefined")
+    Provider<String> jwtIssuerUrlProvider;
+
+    @ConfigProperty(name = "keycloak.credentials.client-id", defaultValue = "undefined")
+    Provider<String> clientIdProvider;
+
+    public String getAccessToken(String userName, String password)
+            throws IOException, InterruptedException {
+        return getAccessToken(userName,password,null);
+    }
+
+    public String getAccessToken(String userName, String password, String clientSecret)
+            throws IOException, InterruptedException {
+        String keycloakTokenEndpoint=jwtIssuerUrlProvider.get()+
+                "/protocol/openid-connect/token";
+        String requestBody = "username=" + userName + "&password=" + password +
+                "&grant_type=password&client_id=" + clientIdProvider.get();
+
+        if (clientSecret != null) {
+            requestBody += "&client_secret=" + clientSecret;
+        }
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request=HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .uri(URI.create(keycloakTokenEndpoint))
+                .header("Content-type","application/x-www-form-urlencoded")
+                .build();
+        HttpResponse<String> response= client.send(request, HttpResponse.BodyHandlers.ofString());
+        String accessToken = "";
+        if(response.statusCode() == 200){
+            ObjectMapper mapper = new ObjectMapper();
+            try{
+                accessToken = mapper.readTree(response.body()).get("access_token").textValue();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            throw new UnauthorizedException();
+        }
+        return accessToken;
+    }
 }
--- src/main/java/com/targa/labs/web/UserResource.java
+++ src/main/java/com/targa/labs/web/UserResource.java
@@ -1,2 +1,60 @@
-package com.targa.labs.web;public class UserResource {
+package com.targa.labs.web;
+
+import com.targa.labs.security.TokenService;
+import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Authenticated
+@Path("/user")
+public class UserResource {
+
+    @Inject
+    JsonWebToken jwt;
+
+    @Inject
+    TokenService tokenService;
+
+    @GET
+    @Path("/current/info")
+    public JsonWebToken getCurrentUserInfo(){
+        return jwt;
+    }
+
+    @GET
+    @Path("/current/info/claims")
+    public Map<String,Object> getCurrentUserInfoClaims(){
+        return jwt.getClaimNames().stream()
+                .map(name -> Map.entry(name,jwt.getClaim(name)))
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue));
+    }
+
+    //alternative
+    @GET
+    @Path("/current/info-alternative")
+    public Principal getCurrentUserInfoAlternative(@Context SecurityContext ctx) {
+        return ctx.getUserPrincipal();
+    }
+
+    @PermitAll
+    @POST
+    @Path("/access-token")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAccessToken(@QueryParam("username") String username,
+                                 @QueryParam("password") String password)
+            throws IOException, InterruptedException {
+        return tokenService.getAccessToken(username,password);
+    }
+
 }
